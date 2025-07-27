aadb6d163543a6fce55ca5106bd54b1fbba99acc
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-27 16:28:33 +0530

    ApplyJob component

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -5,6 +5,7 @@ import Navbar from "../components/navbar/Navbar";
 import { assets } from "../assets/assets";
 import kconvert from "k-convert";
 import moment from "moment";
+import "./ApplyJob.css";
 
 function ApplyJob() {
 
@@ -34,27 +35,28 @@ function ApplyJob() {
                 jobData ? (
                     <>
                         <Navbar />
-                        <div>
-                            <div>
-                                <div>
-                                    <div>
-                                        <img src={jobData.companyId.image} alt="" />
-                                        <div>
-                                            <h1>{jobData.title}</h1>
-                                            <div>
-                                                <span>
+
+                        <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto">
+                            <div className="bg-white text-black rounded-lg w-full">
+                                <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl">
+                                    <div className="flex flex-col md:flex-row items-center">
+                                        <img src={jobData.companyId.image} alt="" className="h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border" />
+                                        <div className="text-center md:text-left text-neutral-700">
+                                            <h1 className="text-2xl font-medium sm:text-4xl">{jobData.title}</h1>
+                                            <div className="flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2">
+                                                <span className="flex items-center gap-1">
                                                     <img src={assets.suitcase_icon} alt="" />
                                                     {jobData.companyId.name}
                                                 </span>
-                                                <span>
+                                                <span className="flex items-center gap-1">
                                                     <img src={assets.location_icon} alt="" />
                                                     {jobData.location}
                                                 </span>
-                                                <span>
+                                                <span className="flex items-center gap-1">
                                                     <img src={assets.person_icon} alt="" />
                                                     {jobData.level}
                                                 </span>
-                                                <span>
+                                                <span className="flex items-center gap-1">
                                                     <img src={assets.money_icon} alt="" />
                                                     CTC: {kconvert.convertTo(jobData.salary)}
                                                 </span>
@@ -62,9 +64,17 @@ function ApplyJob() {
                                         </div>
                                     </div>
 
-                                    <div>
-                                        <button>Apply Now</button>
-                                        <p>Posted {moment(jobData.date).fromNow()}</p>
+                                    <div className="flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center">
+                                        <button className="bg-blue-600 text-white p-2.5 px-10 rounded cursor-pointer">Apply Now</button>
+                                        <p className="mt-1 text-gray-600">Posted {moment(jobData.date).fromNow()}</p>
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col lg:flex-row justify-between items-start">
+                                    <div className="w-full lg:w-2/3">
+                                        <h2 className="font-bold text-2xl mb-4">Job Description</h2>
+                                        <div dangerouslySetInnerHTML={{__html: jobData.description}} className="rich-text"></div>
+                                        <button className="bg-blue-600 text-white p-2.5 px-10 rounded cursor-pointer mt-10">Apply Now</button>
                                     </div>
                                 </div>
                             </div>
