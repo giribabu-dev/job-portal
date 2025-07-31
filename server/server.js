81908bd323c808980eb39dea6aa50074823cb49e
giribabu-a <giribabuannapareddi@gmail.com>
2025-07-31 19:54:54 +0530

    server and database connection

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+import connectDB from "./config/db.js"
+
+// Initialize Express
+const app = express()
+
+// Connect to database
+await connectDB()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => res.send('API Working!'))
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
