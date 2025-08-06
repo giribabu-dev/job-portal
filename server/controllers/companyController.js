4bc247e6fd89120d418746282e8de61172c98d13
giribabu-a <giribabuannapareddi@gmail.com>
2025-08-06 19:46:17 +0530

    Company signup-signin API

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,5 +1,5 @@
 import Company from "../models/Company.js";
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import gererateToken from "../utils/generateToken.js";
 
@@ -28,8 +28,9 @@ export const registerCompany = async (req, res) => {
             name,
             email,
             password: hashPassword,
-            image: imageUpload
+            image: imageUpload.secure_url
         })
+        await company.save()
 
         res.json({
             success: true,
@@ -43,16 +44,41 @@ export const registerCompany = async (req, res) => {
         })
     }
     catch (error) {
-        res.json({
-            success: false,
-            message: error.message
-        })
+        res.json({ success: false, message: error.message })
     }
 }
 
 // Company login
 export const loginCompany = async (req, res) => {
 
+    const { email, password } = req.body
+    try {
+        const isCompanyExists = await Company.findOne({ email })
+
+        if (!isCompanyExists) {
+            return res.json({ success: false, message: "Invalid email or password" })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, isCompanyExists.password)
+        if (isPasswordMatch) {
+            res.json({
+                success: true,
+                company: {
+                    _id: isCompanyExists._id,
+                    name: isCompanyExists.name,
+                    email: isCompanyExists.email,
+                    image: isCompanyExists.image
+                },
+                token: gererateToken(isCompanyExists._id)
+            })
+        }
+        else {
+            res.json({ success: false, message: 'Invalid email or password' })
+        }
+    }
+    catch (error) {
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Get company data
