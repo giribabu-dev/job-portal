b1bc95e4feb4599a17334754db540e2236fa961f
giribabu-a <giribabuannapareddi@gmail.com>
2025-09-06 23:00:17 +0530

    Footer component

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,4 +1,8 @@
 import { assets } from "../../assets/assets";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 function Footer() {
     return (
@@ -13,24 +17,52 @@ function Footer() {
         //         <img src={assets.instagram_icon} alt="" width={38} />
         //     </div>
         // </div>
-        <div className="container px-4 2xl:px-20">
-            <div>
-                <div className="mb-5">
-                    <img src={assets.logo} alt="Logo" width={160} />
-                </div>
-                <div>
-                    <h6 className="font-semibold mb-2 text-base text-gray-500">Connect with us</h6>
-                    <div className="flex gap-3">
-                        <img src={assets.facebook_icon} alt="Facebook icon" width={35} />
-                        <img src={assets.instagram_icon} alt="Instagram icon" width={35} />
-                        <img src={assets.twitter_icon} alt="Twitter icon" width={35} />
+        <div className="container p-4 2xl:px-20">
+            <div className="grid grid-cols-3">
+                <div className="">
+                    <div className="mb-5">
+                        <img src={assets.logo} alt="Logo" width={160} />
+                    </div>
+                    <div>
+                        <h6 className="font-semibold mb-2 text-base">Connect with us</h6>
+                        <div className="flex gap-3">
+                            {/* <img src={assets.facebook_icon} alt="Facebook icon" width={35} /> */}
+                            {/* <img src={assets.instagram_icon} alt="Instagram icon" width={35} /> */}
+                            {/* <img src={assets.twitter_icon} alt="Twitter icon" width={35} /> */}
+                            <FaFacebookSquare className="text-gray-500" />
+                            <FaInstagramSquare className="text-gray-400" />
+                            <FaSquareXTwitter className="text-gray-500" />
+                            <FaLinkedin className="text-gray-500" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="grid grid-cols-3 space-y-3 text-[15px]">
+                    <div>About Us</div>
+                    <div>Help Center</div>
+                    <div>Privacy Policy</div>
+                    <div>Careers</div>
+                    <div>Summons/Notices</div>
+                    <div>Terms & Conditions</div>
+                    <div>Employer Home</div>
+                    <div>Grievances</div>
+                    <div>Fraud Alert</div>
+                    <div>Sitemap</div>
+                    <div>Report Issue</div>
+                    <div>Trust & Safety</div>
+                    <div>Credits</div>
+                </div>
+                <div className="border border-gray-200 rounded p-4">
+                    <h2 className="text-[18px] font-bold">Apply on the go</h2>
+                    <p className="text-[14px]">Get real-time job updates on our App</p>
+                    <div className="flex gap-3 mt-4">
+                        <a href="">
+                            <img src={assets.play_store} alt="" className="h-9" />
+                        </a>
+                        <a href="">
+                            <img src={assets.app_store} alt="" className="h-9" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
